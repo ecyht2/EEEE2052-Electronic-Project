1a9ded445ab6510b8a7831cec061ae4e5fbc4c26
ecyht2 <ecyht2@nottingham.edu.my>
2022-12-02 20:37:31 +0800

    ADC code now uses custom struct.

--- Core/Inc/ADC.h
+++ Core/Inc/ADC.h
@@ -13,10 +13,20 @@
 #include <stdint.h>
 #include "LCD_Display.h"
 
+typedef struct {
+  ADC_HandleTypeDef *hadc;
 
+  /* PRIVATE */
+  struct ADC_param ADC_val;
+  struct FFT_res FFT_val;
+  int flag;
+} ADC;
 
-int ADC(int* , struct ADC_param *ADC_val, struct FFT_res *FFT_val);
-
-
+void ADCInit(ADC *this, const uint64_t adc_buf_len, uint32_t adc_buf[], uint32_t clock_speed,
+	     uint32_t prescaler, uint32_t bit, float sampling_time);
+void ADCStart(ADC *this);
+void ADCStop(ADC *this);
+void ADCHandleCallback(ADC *this);
+float ADCCalculateFrequency(ADC *this, int restart);
 
 #endif /* SRC_ADC_H_ */
--- Core/Src/ADC.c
+++ Core/Src/ADC.c
@@ -13,16 +13,40 @@
 #include "LCD_Display.h"
 
 
-int ADC (int *flag, struct ADC_param *ADC_val, struct FFT_res *FFT_val)
+void ADCInit(ADC *this, const uint64_t adc_buf_len, uint32_t adc_buf[], uint32_t clock_speed,
+	     uint32_t prescaler, uint32_t bit, float sampling_time) {
+  this->ADC_val.adc_buf_len = adc_buf_len;
+  this->ADC_val.adc_buf = adc_buf;
+  this->ADC_val.speed = clock_speed;
+  this->ADC_val.prescaler = prescaler;
+  this->ADC_val.bit = bit;
+  this->ADC_val.sampling_time = sampling_time;
 
-{
-	 char uart_buf[50];
-	 int uart_buf_len;
+  ADCStart(this);
+}
 
+void ADCStart(ADC *this) {
+  HAL_ADC_Start_DMA(this->hadc, this->ADC_val.adc_buf, this->ADC_val.adc_buf_len);
+}
 
-	start_FFT(flag, ADC_val, FFT_val);
-	uart_buf_len = sprintf(uart_buf, "ADC f: %lf", FFT_val->fdominant);
+void ADCStop(ADC *this) {
+  HAL_ADC_Stop_DMA(this->hadc);
+}
 
-	return uart_buf_len;
+void ADCHandleCallback(ADC *this) {
+  this->flag = 1;
+  ADCStop(this);
+}
+
+float ADCCalculateFrequency(ADC *this, int restart) {
+  // Doing FFT
+  if (this->flag == 1) {
+      start_FFT(&(this->flag), &(this->ADC_val), &(this->FFT_val));
+  }
+  // Restarting ADC DMA
+  if (restart == 1) {
+      ADCStart(this);
+  }
+  return this->FFT_val.fdominant;
 }
 
