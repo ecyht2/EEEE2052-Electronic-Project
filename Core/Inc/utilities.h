efc9733e9f39dc3fa92d9153e0193f9103678aa5
ecyht2 <94816144+ecyht2@users.noreply.github.com>
2023-02-21 11:14:20 +0800

    Added BCD converter to header file

--- Core/Inc/utilities.h
+++ Core/Inc/utilities.h
@@ -17,5 +17,6 @@ static const unsigned int c_MPH = 671000000;
 float calculateSpeed(float detected, float transmitted);
 float calculateSpeedMPH(float detected, float transmitted);
 void serial_print(UART_HandleTypeDef *huart, unsigned int buf_len, char *restrict format, ...);
+uint8_t binToBCD(uint8_t number);
 
 #endif /* INC_UTILITIES_H_ */
